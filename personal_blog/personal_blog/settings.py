e82ca2813f31f80606a88e2f77698f500ce6c0fb
Tanvir Mahmud <tanvirmahmud@Tanvirs-MacBook-Air.local>
2023-04-24 00:53:36 +0600

    default email changed

--- personal_blog/personal_blog/settings.py
+++ personal_blog/personal_blog/settings.py
@@ -22,8 +22,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tanvirmahmudfahim1313@gmail.com'
-EMAIL_HOST_PASSWORD = 'iqjmrixszzbzgbxj'
+EMAIL_HOST_USER = 'tanvirmahmud.debugandtest01@gmail.com'
+EMAIL_HOST_PASSWORD = 'xrtlfxebotdinfol'
 
 ALLOWED_HOSTS = []
 
